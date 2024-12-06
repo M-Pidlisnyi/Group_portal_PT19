24d715738de96125c0b2bb618070349645453d34
Дмитро <murchyk2007@gmail.com>
2024-12-06 19:17:09 +0200

    creation of rights in apps.py

--- materialsapp/admin.py
+++ materialsapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Material
 
 # Register your models here.
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('title', 'material_type', 'uploaded_by', 'created_at')
+    list_filter = ('material_type', 'created_at')
+    search_fields = ('title', 'description')
+    readonly_fields = ('created_at', 'updated_at')
--- materialsapp/apps.py
+++ materialsapp/apps.py
@@ -1,6 +1,26 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 
 class MaterialsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'materialsapp'
+
+    def ready(self):
+        # Створення груп
+        admin_group, created = Group.objects.get_or_create(name='Administrators')
+        moderator_group, created = Group.objects.get_or_create(name='Moderators')
+
+        # Додавання прав для моделі Material
+        content_type = ContentType.objects.get(app_label='materials', model='material')
+        
+        # Доступ до редагування, додавання та видалення матеріалів
+        permissions = Permission.objects.filter(content_type=content_type)
+        
+        # Адміністраторам даємо всі права
+        admin_group.permissions.set(permissions)
+
+        # Модераторам лише додавання і редагування
+        moderator_permissions = permissions.filter(codename__in=['add_material', 'change_material'])
+        moderator_group.permissions.set(moderator_permissions)
