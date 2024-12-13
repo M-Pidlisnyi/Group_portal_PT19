6deda4e7f0d9dba4d688b62349ba789237d7bdfe
Kuma-alt <vitalijbetyan@gmail.com>
2024-12-13 19:01:48 +0200

    update

--- porhfollio/forms.py
+++ porhfollio/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Portfolio
+from .models import Portfolio, PortfolioItem
 
 class PortfolioForm(forms.ModelForm):
     class Meta:
         model = Portfolio
+        fields = ['title', 'description']
+
+class PortfolioItemForm(forms.ModelForm):
+    class Meta:
+        model = PortfolioItem
         fields = ['title', 'description', 'screenshot', 'link', 'file']
--- porhfollio/models.py
+++ porhfollio/models.py
@@ -5,6 +5,8 @@ class Portfolio(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="portfolios", verbose_name="Користувач")
     title = models.CharField(max_length=200, verbose_name="Назва")
     description = models.TextField(blank=True, verbose_name="Опис")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено")
 
     def __str__(self):
         return self.title
@@ -17,8 +19,6 @@ class PortfolioItem(models.Model):
     screenshot = models.ImageField(upload_to="portfolio_screenshots/", blank=True, null=True, verbose_name="Скріншот")
     link = models.URLField(blank=True, null=True, verbose_name="Посилання на проект")
     file = models.FileField(upload_to="portfolio_files/", blank=True, null=True, verbose_name="Файл")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено")
 
 
     def __str__(self):
