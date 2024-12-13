e2206c26dc724fec8034af7dfd6b6b23012e3293
ditrotimitskyy <timickijd@gmail.com>
2024-12-13 19:09:43 +0200

    pizza tower the forum (hotfix)

--- forumapp/models.py
+++ forumapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Topic(models.Model):
+
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+
+class Thread(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name="posts")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    post_pic = models.ImageField(upload_to="post_pics/", null=True, blank=True)
