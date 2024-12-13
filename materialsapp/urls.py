1d15819bf2896b240b4d0cd1a994111d15926fac
Дмитро <murchyk2007@gmail.com>
2024-12-13 14:09:27 +0200

    urls.py

--- materialsapp/urls.py
+++ materialsapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.material_list, name='material-list'),
     path('new/', views.material_create, name='material-create'),
-    path('edit/<int:pk>', views.material_edit, name='material-edit'),
-    path('delete/<int:pk>', views.material_delete, name='material-delete'),
+    path('edit/<int:pk>/', views.material_edit, name='material-edit'),
+    path('delete/<int:pk>/', views.material_delete, name='material-delete'),
 ]
