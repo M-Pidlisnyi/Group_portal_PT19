243777a8cfd9cc989852e69146f86dfbb6700427
M-Pidlisnyi <mishapropatriot@gmail.com>
2024-12-13 19:45:54 +0200

    minor fix

--- group_portal/urls.py
+++ group_portal/urls.py
@@ -20,6 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
 
 def home_view(request):
     return HttpResponse("Головна сторінка!")
