a0990f362b5629398b8c08d152a051570abd283e
ditrotimitskyy <timickijd@gmail.com>
2024-12-27 20:36:13 +0200

    finished forum views

--- forumapp/models.py
+++ forumapp/models.py
@@ -7,11 +7,17 @@ class Topic(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
 
+    def __str__(self):
+        return f"{self.title} {self.user} {self.description}"
+
 class Thread(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} made post about {self.title} on {self.topic}"
 
 
 class Post(models.Model):
@@ -21,3 +27,6 @@ class Post(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     post_pic = models.ImageField(upload_to="post_pics/", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.thread} by {self.user}"
--- forumapp/urls.py
+++ forumapp/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import ThreadView, ThreadDetailView, ThreadUpdateView, PostUpdateView
 
 urlpatterns = [
+    path("", ThreadView.as_view(), name="thread_list"),
+    path("detail/<int:pk>/", ThreadDetailView.as_view(), name="thread-detail"),
+    path("update/thread/<int:pk>/", ThreadUpdateView.as_view(), name="thread-update"),
+    path("update/post/<int:pk>/", PostUpdateView.as_view(), name="post-update"),
 ]
